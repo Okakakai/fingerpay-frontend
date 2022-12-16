8869a7588e5b431390e3adf113d69cfd7819e40d
Okakakai <inoue.kai.work@gmail.com>
2022-12-16 15:10:37 +0900

    add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
 import { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ChakraProvider
+        theme={extendTheme({
+          fonts: {
+            heading: "Inter, sans-serif;",
+            body: "Inter, sans-serif;",
+          },
+        })}
+      >
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
