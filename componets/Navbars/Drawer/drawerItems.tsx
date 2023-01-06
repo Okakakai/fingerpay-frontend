e610939aab5307c27a60ed5a396350558e643bc5
Okakakai <inoue.kai.work@gmail.com>
2023-01-06 14:14:12 +0900

    add nav

--- componets/Navbars/Drawer/drawerItems.tsx
+++ componets/Navbars/Drawer/drawerItems.tsx
@@ -1,7 +1,22 @@
-import { Card, chakra, Box, Flex, Icon, Text } from "@chakra-ui/react";
+import {
+  Card,
+  chakra,
+  Box,
+  Flex,
+  Icon,
+  Text,
+  LinkOverlay,
+} from "@chakra-ui/react";
 import { DrawerItemDataSet } from "./variables/drawerItem";
-import { FiChevronRight } from "react-icons/fi";
+import { GetServerSideProps } from "next";
+import { Link } from "@chakra-ui/react";
 
+export const getServerSideProps: GetServerSideProps = async (req) => {
+  const path = req.query.path?.[0] ?? null;
+  return {
+    props: { path },
+  };
+};
 interface Props {
   drawerItemDataSet: DrawerItemDataSet[];
 }
@@ -28,35 +43,37 @@ export const DrawerItems = (props: Props) => {
                 _dark={{ color: "white" }}
               />
             </Flex>
-            <Box mx={-3} py={2} px={4}>
-              <Box mx={3}>
-                <Text
-                  color="#006bcb"
-                  _dark={{
-                    color: "white.400",
-                  }}
-                  letterSpacing="0px"
-                  fontSize="6.3vw"
-                  mb="-2"
-                  p="0px"
-                >
-                  {item.title}
-                </Text>
-                <Text
-                  mt="0"
-                  p="0px"
-                  color="#006bcb"
-                  opacity="0.6"
-                  _dark={{
-                    color: "gray.200",
-                  }}
-                  fontSize="md"
-                >
-                  {/* {item.description} */}
-                  "フレーズを入れる"
-                </Text>
+            <Link href={`/${item.url}`}>
+              <Box mx={-3} py={2} px={4}>
+                <Box mx={3}>
+                  <Text
+                    color="#006bcb"
+                    _dark={{
+                      color: "white.400",
+                    }}
+                    letterSpacing="0px"
+                    fontSize="6.3vw"
+                    mb="-2"
+                    p="0px"
+                  >
+                    {item.title}
+                  </Text>
+                  <Text
+                    mt="0"
+                    p="0px"
+                    color="#006bcb"
+                    opacity="0.6"
+                    _dark={{
+                      color: "gray.200",
+                    }}
+                    fontSize="md"
+                  >
+                    {/* {item.description} */}
+                    "フレーズを入れる"
+                  </Text>
+                </Box>
               </Box>
-            </Box>
+            </Link>
           </Flex>
         );
       })}
