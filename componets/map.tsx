f007923348bfdb8eb981e97ffac903dad3016859
Okakakai <inoue.kai.work@gmail.com>
2023-01-07 12:30:47 +0900

    fix icon

--- componets/map.tsx
+++ componets/map.tsx
@@ -1,6 +1,22 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+//ここでerror が出るが動くのでよし
+// 参考 ↓
+//https://github.com/PaulLeCam/react-leaflet/issues/808
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
+
 const Map = () => {
   return (
     <MapContainer
