83b48cd6dcc02b415974d9f2010c3f3ef2517eff
Okakakai <inoue.kai.work@gmail.com>
2023-01-07 12:43:07 +0900

    remove unrequire partial code

--- componets/map.tsx
+++ componets/map.tsx
@@ -6,11 +6,6 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-//ここでerror が出るが動くのでよし
-// 参考 ↓
-//https://github.com/PaulLeCam/react-leaflet/issues/808
-
-delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
