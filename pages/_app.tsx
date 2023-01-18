12fe32354f4ffe87e556df3b25d02dd04c7f01b9
Okakakai <inoue.kai.work@gmail.com>
2023-01-18 11:07:01 +0900

    add firebase with google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
+
+import { useAuth } from "../lib/auth";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const Auth = ({ children }: Props): JSX.Element => {
+  const isLoading = useAuth();
+
+  return isLoading ? <p>Loading... </p> : children;
+};
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Auth>
+          <Component {...pageProps} />
+        </Auth>
+      </RecoilRoot>
     </ChakraProvider>
   );
 };
